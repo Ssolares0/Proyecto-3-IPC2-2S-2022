749cda1c44ee009275ea19f9195e239b826d75f3
Ssolares0 <3017033530101@ingenieria.usac.edu.gt>
2022-10-14 11:45:50 -0600

    "Modificando FRONTEND, apartado login y signUp"

--- FRONTEND/PROYECTO3/urls.py
+++ FRONTEND/PROYECTO3/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('',views.home,name ='index')
+    path('',views.home,name ='index'),
+    path('signUp/',views.signUp,name='signUp'),
+    path('login/',views.login,name='login'),
 
 ]
--- FRONTEND/PROYECTO3/views.py
+++ FRONTEND/PROYECTO3/views.py
@@ -21,3 +21,42 @@ def home(request):
         print("API no esta corriendo")    
 
     return render(request, "index.html",context=context)
+
+def signUp(request):
+    ctx = {
+        'content':None,
+        'response':None
+    }
+    if request.method == 'POST':
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            f = request.FILES['file']
+            xml_binary = f.read()
+            xml = xml_binary.decode('utf-8')
+            ctx['content'] = xml
+            response = requests.post(endpoint + 'agregarDatos', data=xml_binary)
+            if response.ok:
+                response =requests.get(endpoint + 'buscarmensajes')
+                buscarmsg = response.json()
+                ctx['response'] = buscarmsg
+            else:
+                ctx['response'] = 'El archivo se envio, pero hubo un error en el servidor'
+    else:
+        return render(request, 'signUp.html')
+    return render(request, 'signUp.html', ctx)
+
+def login(request):
+    context = {
+        'datos': [],
+        'title' : 'Home'
+    }
+    try:
+        response =requests.get(endpoint + 'buscardatos')
+        buscardatos = response.json()
+        
+        context['datos']= buscardatos
+       
+    except:
+        print("API no esta corriendo")    
+
+    return render(request, "login.html",context=context)
